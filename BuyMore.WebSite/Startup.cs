fa437e8efd73daae99bf272cf5db180add27964c
Georges Damien <damien.georges@gmail.com>
2016-02-28 21:14:59 +0100

    rebase

---
+++ BuyMore.WebSite/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuyMore.WebSite.Startup))]
+namespace BuyMore.WebSite
+{
+    public partial class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
